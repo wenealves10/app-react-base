a345906d8de5cc8e270fac4a715fce610b0a8b0c
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-21 12:44:55 -0300

    Modifying the code, and adding container

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Button, Title } from './Styled';
+import { Container } from '../../styles/Global';
 
 const isColor = false;
 
 const Login = () => (
-  <>
+  <Container>
     <Title>Hello World</Title>
     <Button isColor={!isColor}>Enviar</Button>
-  </>
+  </Container>
 );
 
 export default Login;
