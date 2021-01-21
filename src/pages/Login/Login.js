9c2146a7593c27c5fe5e54963cd47af6f95c144b
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-21 19:14:06 -0300

    modifying the code, arrow function for function normal

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -4,11 +4,11 @@ import { Container } from '../../styles/Global';
 
 const isColor = false;
 
-const Login = () => (
-  <Container>
-    <Title>Hello World</Title>
-    <Button isColor={!isColor}>Enviar</Button>
-  </Container>
-);
-
-export default Login;
+export default function Login() {
+  return (
+    <Container>
+      <Title>Hello World</Title>
+      <Button isColor={!isColor}>Enviar</Button>
+    </Container>
+  );
+}
