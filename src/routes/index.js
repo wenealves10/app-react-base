0c5e95cc9a2424497a3963bbd54356288c3dd3c2
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-21 13:28:02 -0300

    Adding Private Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import Login from '../pages/Login/Login';
 import Page404 from '../pages/Page404';
+import PrivateRoute from './PrivateRoute';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path="/" component={Login} />
-      <Route path="*" component={Page404} />
+      <PrivateRoute exact path="/" component={Login} />
+      <PrivateRoute path="*" component={Page404} />
     </Switch>
   );
 }
