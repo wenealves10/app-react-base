ebaf3652907c79480ba3d31b1b7d388c281fc65d
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-21 19:18:20 -0300

    React Hooks

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Button, Title } from './Styled';
 import { Container } from '../../styles/Global';
+import axios from '../../services/axios';
 
 const isColor = false;
 
 export default function Login() {
+  React.useEffect(() => {
+    async function getData() {
+      const response = await axios.get();
+      const { data } = response;
+      // eslint-disable-next-line no-console
+      console.log(data);
+    }
+    getData();
+  }, []);
   return (
     <Container>
       <Title>Hello World</Title>
