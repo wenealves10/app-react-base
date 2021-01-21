0a23e0d34eb5425a8aa99a5ba793fab5c0fa2661
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-21 14:02:42 -0300

    React Toastify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import history from './services/history';
 import Header from './components/Header';
 import Global from './styles/Global';
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Routes />
       <Global />
+      <ToastContainer autoClose={3000} className="toast-container" />
     </Router>
   );
 }
