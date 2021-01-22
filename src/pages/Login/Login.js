5be909fbebff3922e0a8482967d44d151f0bcc0c
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-21 21:09:40 -0300

    Testing the Redux

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Button, Title } from './Styled';
 import { Container } from '../../styles/Global';
 import axios from '../../services/axios';
 
-const isColor = false;
-
 export default function Login() {
+  const dispatch = useDispatch();
+  const isColor = useSelector((state) => state.buttonClick);
   React.useEffect(() => {
     async function getData() {
       const response = await axios.get();
@@ -15,10 +16,20 @@ export default function Login() {
     }
     getData();
   }, []);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: 'BUTTON_CLICK',
+    });
+  };
+
   return (
     <Container>
       <Title>Hello World</Title>
-      <Button isColor={!isColor}>Enviar</Button>
+      <Button isColor={!isColor} onClick={handleClick}>
+        Enviar
+      </Button>
     </Container>
   );
 }
