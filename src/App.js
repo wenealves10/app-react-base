08c4895e1222919a11463d76b4735d73d81399a0
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-22 21:12:04 -0300

    Modifying the code and Configuring the Redux Persist

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import history from './services/history';
 import Header from './components/Header';
 import Global from './styles/Global';
 import Routes from './routes';
-import store from './store';
+import store, { persistor } from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Header />
-        <Routes />
-        <Global />
-        <ToastContainer autoClose={3000} className="toast-container" />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Header />
+          <Routes />
+          <Global />
+          <ToastContainer autoClose={3000} className="toast-container" />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
